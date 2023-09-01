bada22b613f3c37c6122a1c7ef2a6c7e2d499f13
Peisong-Zheng <pz285@cam.ac.uk>
2023-09-01 10:19:00 +0100

    rewrite some functions

--- toolbox/GMM_functions.py
+++ toolbox/GMM_functions.py
@@ -1,167 +1,167 @@
-# function to fit a 2D Gaussian mixture model to the 'sat' data variable of an xarray dataset  
-# #####################################################################################
-import xarray as xr
+# # function to fit a 2D Gaussian mixture model to the 'sat' data variable of an xarray dataset  
+# # #####################################################################################
+# import xarray as xr
 
 
-def cal_anomalies(ds_sat, years):
-    """calculate the temperature anomalies by subtracting the mean over the specified years from the 'sat' data variable of an xarray dataset.
+# def cal_anomalies(ds_sat, years):
+#     """calculate the temperature anomalies by subtracting the mean over the specified years from the 'sat' data variable of an xarray dataset.
 
-    Parameters
-    ----------
-    ds_sat : xarray.Dataset
-        An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
-    years : int
-        The number of years to calculate the mean over.
+#     Parameters
+#     ----------
+#     ds_sat : xarray.Dataset
+#         An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
+#     years : int
+#         The number of years to calculate the mean over.
 
-    Returns
-    -------
-    xarray.Dataset
-        The updated xarray dataset with a new 'sat_anomalies' data variable.
-    """
-    # Select the specified years of the record
-    ds_years = ds_sat.sel(age=slice(None, years))
+#     Returns
+#     -------
+#     xarray.Dataset
+#         The updated xarray dataset with a new 'sat_anomalies' data variable.
+#     """
+#     # Select the specified years of the record
+#     ds_years = ds_sat.sel(age=slice(None, years))
 
-    # Calculate the mean over the specified years
-    mean_years = ds_years['sat'].mean(dim='age')
+#     # Calculate the mean over the specified years
+#     mean_years = ds_years['sat'].mean(dim='age')
 
-    # Compute the anomalies by subtracting the mean from the 'sat' variable
-    anomalies_sat = ds_sat['sat'] - mean_years
+#     # Compute the anomalies by subtracting the mean from the 'sat' variable
+#     anomalies_sat = ds_sat['sat'] - mean_years
 
-    # Add the anomalies as a new data variable in the dataset
-    ds_sat['sat_anomalies'] = anomalies_sat
+#     # Add the anomalies as a new data variable in the dataset
+#     ds_sat['sat_anomalies'] = anomalies_sat
 
-    return ds_sat
+#     return ds_sat
 
-###############################################################################
-import numpy as np
-import xarray as xr
-import Legacy_code.GMM_functions as gf
+# ###############################################################################
+# import numpy as np
+# import xarray as xr
+# import Legacy_code.GMM_functions as gf
 
-def cal_weighted_anomalies(ds_sat):
-    """calculate the weighted temperature anomalies by multiplying the 'sat_anomalies' data variable with the weight based on cos(lat).
-    Parameters
-    ----------
-    ds_sat : xarray.Dataset
-        An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
+# def cal_weighted_anomalies(ds_sat):
+#     """calculate the weighted temperature anomalies by multiplying the 'sat_anomalies' data variable with the weight based on cos(lat).
+#     Parameters
+#     ----------
+#     ds_sat : xarray.Dataset
+#         An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
         
-    Returns
-    -------
-    xarray.Dataset
-        The updated xarray dataset with a new 'sat_anomalies_weighted' data variable.
-    """
-    ds_sat = cal_anomalies(ds_sat, 2000)
-    # Calculate the weight based on cos(lat)
-    weight = np.cos(np.deg2rad(ds_sat['lat']))
+#     Returns
+#     -------
+#     xarray.Dataset
+#         The updated xarray dataset with a new 'sat_anomalies_weighted' data variable.
+#     """
+#     ds_sat = cal_anomalies(ds_sat, 2000)
+#     # Calculate the weight based on cos(lat)
+#     weight = np.cos(np.deg2rad(ds_sat['lat']))
 
-    # Multiply 'sat_anomalies' with the weight
-    sat_anomalies_weighted = ds_sat['sat_anomalies'] * weight
+#     # Multiply 'sat_anomalies' with the weight
+#     sat_anomalies_weighted = ds_sat['sat_anomalies'] * weight
 
-    # Add 'sat_anomalies_weighted' as a new data variable in the dataset
-    ds_sat['sat_anomalies_weighted'] = sat_anomalies_weighted
+#     # Add 'sat_anomalies_weighted' as a new data variable in the dataset
+#     ds_sat['sat_anomalies_weighted'] = sat_anomalies_weighted
 
-    return ds_sat
+#     return ds_sat
 
   
-################################################################################
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from scipy.stats import zscore
-import numpy as np
-from sklearn.mixture import GaussianMixture
-import xarray as xr
-import matplotlib.pyplot as plt
+# ################################################################################
+# import pandas as pd
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import PCA
+# from scipy.stats import zscore
+# import numpy as np
+# from sklearn.mixture import GaussianMixture
+# import xarray as xr
+# import matplotlib.pyplot as plt
 
-colors = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+# colors = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
 
-def fit_gmm(ds_sat, variable='sat',n_pc=2, n_components=3):
-    """Fit a 2D Gaussian mixture model to the 'sat' data variable of an xarray dataset.
+# def fit_gmm(ds_sat, variable='sat',n_pc=2, n_components=3):
+#     """Fit a 2D Gaussian mixture model to the 'sat' data variable of an xarray dataset.
 
-    Parameters
-    ----------
-    ds_sat : xarray.Dataset
-        An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
-    n_pc : int
-        Number of principal components to retain after PCA
-    n_components : int
-        Number of components in the Gaussian mixture model
+#     Parameters
+#     ----------
+#     ds_sat : xarray.Dataset
+#         An xarray dataset containing a 'sat' data variable with dimensions (age, lat, lon).
+#     n_pc : int
+#         Number of principal components to retain after PCA
+#     n_components : int
+#         Number of components in the Gaussian mixture model
 
-    Returns
-    -------
-    xarray.Dataset
-        The updated xarray dataset with a new 'class_label' data variable.
+#     Returns
+#     -------
+#     xarray.Dataset
+#         The updated xarray dataset with a new 'class_label' data variable.
 
-    """
-    if n_components > 10:
-        raise ValueError('n_components cannot be greater than 10, otherwise the colors will repeat.')
+#     """
+#     if n_components > 10:
+#         raise ValueError('n_components cannot be greater than 10, otherwise the colors will repeat.')
     
-    ds_sat = cal_weighted_anomalies(ds_sat)
-    # ds_sat = cal_anomalies(ds_sat, 2000)
+#     ds_sat = cal_weighted_anomalies(ds_sat)
+#     # ds_sat = cal_anomalies(ds_sat, 2000)
 
-    # get all the sat from ds_sat and put it to a ndarray
-    # sat = ds_sat['sat'].values
-    # sat = ds_sat['sat_anomalies_weighted'].values
-    sat = ds_sat[variable].values
+#     # get all the sat from ds_sat and put it to a ndarray
+#     # sat = ds_sat['sat'].values
+#     # sat = ds_sat['sat_anomalies_weighted'].values
+#     sat = ds_sat[variable].values
 
-    sat_shape = sat.shape
+#     sat_shape = sat.shape
 
-    # reshape the sat to a 2D array
-    sat = sat.reshape(sat_shape[0], sat_shape[1]*sat_shape[2])
-    sat = sat.T
+#     # reshape the sat to a 2D array
+#     sat = sat.reshape(sat_shape[0], sat_shape[1]*sat_shape[2])
+#     sat = sat.T
 
-    # Normalize the data
-    scaler = StandardScaler()
-    sat_scaled = scaler.fit_transform(sat)
+#     # Normalize the data
+#     scaler = StandardScaler()
+#     sat_scaled = scaler.fit_transform(sat)
 
-    # PCA
-    pca_sat = PCA(n_components=n_pc)
-    columns = [f'pc {i}' for i in range(1, n_pc+1)]
-    PCs = pca_sat.fit_transform(sat_scaled.T)
+#     # PCA
+#     pca_sat = PCA(n_components=n_pc)
+#     columns = [f'pc {i}' for i in range(1, n_pc+1)]
+#     PCs = pca_sat.fit_transform(sat_scaled.T)
 
-    # calculate the PCA scores for the original unscaled data
-    sat_scores = sat_scaled.dot(zscore(PCs))
+#     # calculate the PCA scores for the original unscaled data
+#     sat_scores = sat_scaled.dot(zscore(PCs))
 
-    # create a 2D GMM model
-    gmm_model = GaussianMixture(n_components=n_components, covariance_type='full')
+#     # create a 2D GMM model
+#     gmm_model = GaussianMixture(n_components=n_components, covariance_type='full')
 
-    # fit the model to the two columns of PCA scores
-    gmm_model.fit(sat_scores)
+#     # fit the model to the two columns of PCA scores
+#     gmm_model.fit(sat_scores)
 
-    # get the predicted class labels for each data point
-    class_labels = gmm_model.predict(sat_scores)
+#     # get the predicted class labels for each data point
+#     class_labels = gmm_model.predict(sat_scores)
 
-    # add the class labels to the xarray dataset
-    ds_sat['class_label'] = (('lat', 'lon'), class_labels.reshape(sat_shape[1], sat_shape[2]))
+#     # add the class labels to the xarray dataset
+#     ds_sat['class_label'] = (('lat', 'lon'), class_labels.reshape(sat_shape[1], sat_shape[2]))
 
-    # plot the results
-    fig, ax = plt.subplots(1, 2, figsize=(8, 4), dpi=300)
+#     # plot the results
+#     fig, ax = plt.subplots(1, 2, figsize=(8, 4), dpi=300)
 
-    # plot the scatter plot of the two columns
-    for i in range(n_components):
-        mask = class_labels == i
-        # ax[0].scatter(sat_scores[:, 0][mask], sat_scores[:, 1][mask], s=10, alpha=0.5, color=colors[i % len(colors)])
-        ax[0].scatter(sat_scores[:, 0][mask], sat_scores[:, 1][mask], s=10, alpha=0.5, color=colors[i])
+#     # plot the scatter plot of the two columns
+#     for i in range(n_components):
+#         mask = class_labels == i
+#         # ax[0].scatter(sat_scores[:, 0][mask], sat_scores[:, 1][mask], s=10, alpha=0.5, color=colors[i % len(colors)])
+#         ax[0].scatter(sat_scores[:, 0][mask], sat_scores[:, 1][mask], s=10, alpha=0.5, color=colors[i])
 
-    # plot the contour plot of the fitted GMM
-    x, y = np.meshgrid(np.linspace(np.min(sat_scores[:, 0]), np.max(sat_scores[:, 0]), 100),
-                       np.linspace(np.min(sat_scores[:, 1]), np.max(sat_scores[:, 1]), 100))
-    XX = np.array([x.ravel(), y.ravel()]).T
-    Z = -gmm_model.score_samples(XX)
-    Z = Z.reshape(x.shape)
-    ax[1].contour(x, y, Z, cmap='coolwarm_r')
+#     # plot the contour plot of the fitted GMM
+#     x, y = np.meshgrid(np.linspace(np.min(sat_scores[:, 0]), np.max(sat_scores[:, 0]), 100),
+#                        np.linspace(np.min(sat_scores[:, 1]), np.max(sat_scores[:, 1]), 100))
+#     XX = np.array([x.ravel(), y.ravel()]).T
+#     Z = -gmm_model.score_samples(XX)
+#     Z = Z.reshape(x.shape)
+#     ax[1].contour(x, y, Z, cmap='coolwarm_r')
 
-    # Add labels and title
-    ax[0].set_xlabel('PC 1')
-    ax[0].set_ylabel('PC 2')
-    ax[1].set_xlabel('PC 1')
-    ax[1].set_ylabel('PC 2')
-    ax[0].set_title('Scatter plot of PCA scores')
-    ax[1].set_title('Contour plot of fitted GMM')
+#     # Add labels and title
+#     ax[0].set_xlabel('PC 1')
+#     ax[0].set_ylabel('PC 2')
+#     ax[1].set_xlabel('PC 1')
+#     ax[1].set_ylabel('PC 2')
+#     ax[0].set_title('Scatter plot of PCA scores')
+#     ax[1].set_title('Contour plot of fitted GMM')
 
-    plt.tight_layout()
-    plt.show()
+#     plt.tight_layout()
+#     plt.show()
 
-    #################################################################################################
+#################################################################################################
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
